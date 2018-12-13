1f778d39ec79bd86a4911b01cfdede65a229c258
amirul-inc <it.amirulfatta@gmail.com>
2018-12-13 16:08:07 +0700

    create user router

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const UserController = require('../controllers/User')
 
 /* GET users listing. */
+router.post('/register', UserController.create)
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
