e6c86f2c5b8158132e0278c09cf49fb61501256b
puancoffee <puanngopi@gmail.com>
2019-10-18 17:16:34 +0700

    add delete by id in guest

--- routes/guest.js
+++ routes/guest.js
@@ -5,6 +5,10 @@ const GuestController = require('../controllers/Guest')
 
 /* POST Guest . */
 router.post('/guest', GuestController.create)
+/* PUT Guest . */
+router.put('/:guestId', GuestController.updateById)
+/* DELETE Guest . */
+router.put('/:guestId', GuestController.deleteByid)
 /* GET Guest Listing. */
 router.get('/guest', GuestController.getAll)
 
